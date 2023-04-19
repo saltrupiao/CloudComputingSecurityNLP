61444d832de8d1ec525ddab170a33b9d77de4a67
saltrupiao <strupiano@oakland.edu>
2023-04-18 22:33:15 -0400

    visualize_data3 addition - get rid of loop and csv file interaction, use python only to visualize data

--- main.py
+++ main.py
@@ -15,9 +15,30 @@ nlp = spacy.load("en_core_web_sm")
 nlp = en_core_web_trf.load()
 
 
-def spacy_init():
-    doc = nlp("This is a sentence.")
-    print([(w.text, w.pos_) for w in doc])
+def spacy_init_list_tst():
+    # doc = nlp("This is a sentence.")
+    # print([(w.text, w.pos_) for w in doc])
+
+    i = 0
+    j = 0
+    urls = []
+    while i == 0:
+        print("value for J: ")
+        print(j)
+        print("Please enter URLs for NLP Processing with Spacy Library: ")
+        urls.append(input())
+        print("Any other URLs for processing? Enter Y for yes, and N for no: ")
+        decision = input()
+        if decision == "Y":
+            i = 0
+        else:
+            i = 1
+        j += 1
+        print("value of J: ")
+        print(j)
+
+    print("URLs List: ")
+    print(urls)
 
 
 def visualize_data():
@@ -35,8 +56,33 @@ def visualize_data2():
     nlp = spacy.load('en_core_web_sm')
     nlp.add_pipe('spacytextblob')
 
+    i = 0
+    j = 0
+    urls = []
+    while i == 0:
+        print("value for J: ")
+        print(j)
+        print("Please enter URLs for NLP Processing with Spacy Library: ")
+        urls.append(input())
+        print("Any other URLs for processing? Enter Y for yes, and N for no: ")
+        decision = input()
+        if decision == "Y":
+            i = 0
+        else:
+            i = 1
+        j += 1
+        print("value of J: ")
+        print(j)
+
+    print("URLs List: ")
+    print(urls)
+
     file_in = pd.read_csv("cc_urls_v1.csv")
+    print("printing variable file_in")
+    print(file_in)
     ccURLs = file_in["Address"].tolist()
+    print("printing ccURLs...")
+    print(ccURLs)
     url_sent_score = []
     url_sent_label = []
     total_pos = []
@@ -45,54 +91,168 @@ def visualize_data2():
     print("Enumerate CCURLs Value: ")
     print(enumerate(ccURLs))
 
-    for count, x in enumerate(ccURLs):
-        url = x
-        print("Running and scraping for the following URL: ", url)
-        headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
-        res = requests.get(url,headers=headers)
-        html_page = res.text
+    # for count, x in enumerate(ccURLs):
 
-        soup = BeautifulSoup(html_page, 'html.parser')
-        for script in soup(["script", "style","meta","label","header","footer"]):
-            script.decompose()
-        page_text = (soup.get_text()).lower()
-        page_text = page_text.strip().replace("  ", "")
-        page_text = "".join([s for s in page_text.splitlines(True) if s.strip("\r\n")])
 
-        doc = nlp(page_text)
-        sentiment = doc._.blob.polarity
-        sentiment = round(sentiment, 2)
+    url = "https://news.ycombinator.com/item?id=19484167"
+    print("Running and scraping for the following URL: ", url)
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
+    res = requests.get(url,headers=headers)
+    html_page = res.text
 
-        if sentiment > 0:
-            sent_label = "Positive"
+    soup = BeautifulSoup(html_page, 'html.parser')
+    for script in soup(["script", "style","meta","label","header","footer"]):
+        script.decompose()
+    page_text = (soup.get_text()).lower()
+    page_text = page_text.strip().replace("  ", "")
+    page_text = "".join([s for s in page_text.splitlines(True) if s.strip("\r\n")])
+
+    doc = nlp(page_text)
+    sentiment = doc._.blob.polarity
+    sentiment = round(sentiment, 2)
+
+    if sentiment > 0:
+        sent_label = "Positive"
+    else:
+        sent_label = "Negative"
+
+    url_sent_label.append(sent_label)
+    url_sent_score.append(sentiment)
+
+    positive_words = []
+    negative_words = []
+
+    for x in doc._.blob.sentiment_assessments.assessments:
+        if x[1] > 0:
+            positive_words.append(x[0][0])
+        elif x[1] < 0:
+            negative_words.append(x[0][0])
         else:
-            sent_label = "Negative"
+            pass
 
-        url_sent_label.append(sent_label)
-        url_sent_score.append(sentiment)
+    total_pos.append(', '.join(set(positive_words)))
+    total_neg.append(', '.join(set(negative_words)))
 
-        positive_words = []
-        negative_words = []
+    print(file_in)
+    print(url_sent_score)
+    print(url_sent_label)
+    print(total_pos)
+    print(total_neg)
 
-        for x in doc._.blob.sentiment_assessments.assessments:
-            if x[1] > 0:
-                positive_words.append(x[0][0])
-            elif x[1] < 0:
-                negative_words.append(x[0][0])
-            else:
-                pass
-
-        total_pos.append(', '.join(set(positive_words)))
-        total_neg.append(', '.join(set(negative_words)))
-
-        file_in["Sentiment Score"] = pd.Series([url_sent_score])
-        file_in["Sentiment Label"] = pd.Series([url_sent_label])
-        file_in["Positive Words"] = pd.Series([total_pos])
-        file_in["Negative Words"] = pd.Series([total_neg])
+    file_in["Sentiment Score"] = pd.Series([url_sent_score])
+    file_in["Sentiment Label"] = pd.Series([url_sent_label])
+    file_in["Positive Words"] = pd.Series([total_pos])
+    file_in["Negative Words"] = pd.Series([total_neg])
 
 
     # optional export to CSV
-    file_in.to_csv('sentiment_trial7 .csv')
+    file_in.to_csv('sentiment_trial8.csv')
+    file_in
+
+    # print(doc._.blob.polarity)
+    # print(doc._.blob.subjectivity)
+    # print(doc._.blob.sentiment_assessments.assessments)
+    # sentence_spans = list(doc.sents)
+    # displacy.serve(sentence_spans, style='ent')
+
+
+def visualize_data3():
+    import spacy
+    from spacy import displacy
+    nlp = spacy.load('en_core_web_sm')
+    nlp.add_pipe('spacytextblob')
+
+    i = 0
+    j = 0
+    urls = []
+    while i == 0:
+        print("value for J: ")
+        print(j)
+        print("Please enter URLs for NLP Processing with Spacy Library: ")
+        urls.append(input())
+        print("Any other URLs for processing? Enter Y for yes, and N for no: ")
+        decision = input()
+        if decision == "Y":
+            i = 0
+        else:
+            i = 1
+        j += 1
+        print("value of J: ")
+        print(j)
+
+    print("URLs List: ")
+    print(urls)
+
+    file_in = pd.read_csv("cc_urls_v1.csv")
+    print("printing variable file_in")
+    print(file_in)
+    ccURLs = file_in["Address"].tolist()
+    print("printing ccURLs...")
+    print(ccURLs)
+    url_sent_score = []
+    url_sent_label = []
+    total_pos = []
+    total_neg = []
+
+    print("Enumerate CCURLs Value: ")
+    print(enumerate(ccURLs))
+
+    # for count, x in enumerate(ccURLs):
+
+
+    url = "https://news.ycombinator.com/item?id=19484167"
+    print("Running and scraping for the following URL: ", url)
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
+    res = requests.get(url,headers=headers)
+    html_page = res.text
+
+    soup = BeautifulSoup(html_page, 'html.parser')
+    for script in soup(["script", "style","meta","label","header","footer"]):
+        script.decompose()
+    page_text = (soup.get_text()).lower()
+    page_text = page_text.strip().replace("  ", "")
+    page_text = "".join([s for s in page_text.splitlines(True) if s.strip("\r\n")])
+
+    doc = nlp(page_text)
+    sentiment = doc._.blob.polarity
+    sentiment = round(sentiment, 2)
+
+    if sentiment > 0:
+        sent_label = "Positive"
+    else:
+        sent_label = "Negative"
+
+    url_sent_label.append(sent_label)
+    url_sent_score.append(sentiment)
+
+    positive_words = []
+    negative_words = []
+
+    for x in doc._.blob.sentiment_assessments.assessments:
+        if x[1] > 0:
+            positive_words.append(x[0][0])
+        elif x[1] < 0:
+            negative_words.append(x[0][0])
+        else:
+            pass
+
+    total_pos.append(', '.join(set(positive_words)))
+    total_neg.append(', '.join(set(negative_words)))
+
+    print(file_in)
+    print(url_sent_score)
+    print(url_sent_label)
+    print(total_pos)
+    print(total_neg)
+
+    file_in["Sentiment Score"] = pd.Series([url_sent_score])
+    file_in["Sentiment Label"] = pd.Series([url_sent_label])
+    file_in["Positive Words"] = pd.Series([total_pos])
+    file_in["Negative Words"] = pd.Series([total_neg])
+
+
+    # optional export to CSV
+    file_in.to_csv('sentiment_trial8.csv')
     file_in
 
     # print(doc._.blob.polarity)
@@ -104,7 +264,7 @@ def visualize_data2():
 
 def main():
     print("Running Init Function\n")
-    visualize_data2()
+    spacy_init()
 
 
 main()
